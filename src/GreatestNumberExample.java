733b3d7fee5f74decad5ad1941b582990b56425e
ballaaddict40 <ballaaddict40@localhost>
2021-02-22 12:31:35 -0500

    Greatest Number Example program.

---
+++ src/GreatestNumberExample.java
@@ -0,0 +1,22 @@
+
+public class GreatestNumberExample {
+
+	public static void main(String[] args) {
+		int num1= 10;
+		int num2= 25;
+		int num3= 30;
+		
+		if (num1>num2)
+		if (num1>num3)
+	System.out.println("The greatest number is:" + num1);
+		
+		if (num2>num1)
+		if (num2>num3)
+	System.out.println("The greatest number is:" +num2);
+		
+		if (num3>num1)
+		if (num3>num2)
+	System.out.println("The greatest number is:" +num3);
+		
+	}
+}
